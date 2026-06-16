b0794d55c4a4bd934b3bc46f236ab17c4b2deff2
Raahul Sallagunta <raahul@Raahuls-MacBook-Pro.local>
2026-06-16 12:49:19 -0700

    Fix SmartcartApplicationTests using EmbeddedKafka

--- src/test/java/com/smartcart/smartcart/SmartcartApplicationTests.java
+++ src/test/java/com/smartcart/smartcart/SmartcartApplicationTests.java
@@ -5,15 +5,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @TestPropertySource(properties = {
-		"spring.kafka.bootstrap-servers=localhost:9092",
+		"spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}",
 		"spring.kafka.properties.security.protocol=PLAINTEXT",
 		"spring.data.redis.host=localhost",
 		"spring.data.redis.port=6379",
 		"spring.cache.type=simple"
 })
+@EmbeddedKafka(partitions = 1)
 @DisplayName("Application Context Tests")
 class SmartcartApplicationTests {
 
